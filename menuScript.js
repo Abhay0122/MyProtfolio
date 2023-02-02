072f703e12e8bde0c7fd24897e13dd36b403c23c
Abhay0122 <parmarabhay2201@gmail.com>
2023-02-02 18:29:44 +0530

    third commit

--- menuScript.js
+++ menuScript.js
@@ -10,3 +10,30 @@ menubtn.addEventListener("click", () => {
 menuCross.addEventListener("click", () => {
     menuOverlay.style.transform = "translatex(-100%)";
 });
+
+
+// cursor
+const cursor = document.querySelector('.cursor');
+document.addEventListener("mousemove", (e) => {
+    var y = e.clientY
+    var x = e.clientX
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+});
+
+const p = document.querySelector('.paragraph');
+const p2 = document.querySelector('.paragraph-2');
+
+p.addEventListener("mouseenter", () => {
+    cursor.style.transform = "scale(6)"
+})
+p.addEventListener("mouseleave", () => {
+    cursor.style.transform = "scale(1)"
+})
+// // -----
+// p2.addEventListener("mouseenter", () => {
+//     cursor.style.transform = "scale(6)"
+// })
+// p2.addEventListener("mouseleave", () => {
+//     cursor.style.transform = "scale(1)"
+// })
